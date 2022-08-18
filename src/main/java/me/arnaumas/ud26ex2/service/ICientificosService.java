2751f5003bc5c30a81ebe22cca44e6b4380dccea
Arini11 <elarnaumc@gmail.com>
2022-08-18 11:12:27 +0200

    ex2

---
+++ src/main/java/me/arnaumas/ud26ex2/service/ICientificosService.java
@@ -0,0 +1,19 @@
+package me.arnaumas.ud26ex2.service;
+
+import java.util.List;
+
+import me.arnaumas.ud26ex2.dto.Cientificos;
+
+public interface ICientificosService {
+	public List<Cientificos> listarCientificos();
+	
+	public Cientificos guardarCientifico(Cientificos cientificos);
+	
+	public Cientificos cientificoXDni(String dni);
+	
+	public Cientificos actualizarCientifico(Cientificos cientificos);
+	
+	public void eliminarCientificos(String dni);
+
+	public List<Cientificos> listarClienteDni(String dni);
+}
---
+++ src/main/java/me/arnaumas/ud26ex2/service/IProyectoService.java
@@ -0,0 +1,18 @@
+package me.arnaumas.ud26ex2.service;
+
+import java.util.List;
+
+import me.arnaumas.ud26ex2.dto.Proyecto;
+
+public interface IProyectoService {
+	
+	public List<Proyecto> listarProyecto();
+	
+	public Proyecto guardarProyecto(Proyecto proyecto);
+	
+	public Proyecto proyectoXID(String id);
+	
+	public Proyecto actualizarProyecto(Proyecto proyecto);
+	
+	public void eliminarProyecto(String id);
+}
