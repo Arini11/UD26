9b64b1d98877268c1c50148c0e4a4da715b0e341
Arini11 <elarnaumc@gmail.com>
2022-08-23 09:05:36 +0200

    lombok integrat amb security token

--- src/main/java/me/arnaumas/ud26ex1/dto/Proveedores.java
+++ src/main/java/me/arnaumas/ud26ex1/dto/Proveedores.java
@@ -6,56 +6,26 @@ import javax.persistence.*;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "proveedores")
-
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Proveedores {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private String id;
+	private @Getter @Setter String id;
 
-	private String nombre;
+	private @Getter @Setter String nombre;
 
 	@JsonIgnore
 	@OneToMany(mappedBy = "proveedor")
-	private List<Suministra> suministra;
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public List<Suministra> getSuministra() {
-		return suministra;
-	}
-
-	public void setSuministra(List<Suministra> suministra) {
-		this.suministra = suministra;
-	}
-
-	public Proveedores(String id, String nombre, List<Suministra> suministra) {
-		super();
-		this.id = id;
-		this.nombre = nombre;
-		this.suministra = suministra;
-	}
-
-	public Proveedores() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	
+	private @Getter @Setter List<Suministra> suministra;
 }
--- src/main/java/me/arnaumas/ud26ex1/dto/Suministra.java
+++ src/main/java/me/arnaumas/ud26ex1/dto/Suministra.java
@@ -1,77 +1,30 @@
 package me.arnaumas.ud26ex1.dto;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
-
+import lombok.*;
+import lombok.ToString.Exclude;
 
 @Entity
 @Table(name = "suministra")
-
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Suministra {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
+	private @Getter @Setter int id;
 
+	@Exclude
 	@ManyToOne
 	@JoinColumn(name = "codigo_pieza")
-	private Piezas pieza;
+	private @Getter @Setter Piezas pieza;
 
+	@Exclude
 	@ManyToOne
 	@JoinColumn(name = "id_proveedor")
-	private Proveedores proveedor;
+	private @Getter @Setter Proveedores proveedor;
 
-	private int precio;
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public Piezas getPieza() {
-		return pieza;
-	}
-
-	public void setPieza(Piezas pieza) {
-		this.pieza = pieza;
-	}
-
-	public Proveedores getProveedor() {
-		return proveedor;
-	}
-
-	public void setProveedor(Proveedores proveedor) {
-		this.proveedor = proveedor;
-	}
-
-	public int getPrecio() {
-		return precio;
-	}
-
-	public void setPrecio(int precio) {
-		this.precio = precio;
-	}
-
-	public Suministra(int id, Piezas pieza, Proveedores proveedor, int precio) {
-		super();
-		this.id = id;
-		this.pieza = pieza;
-		this.proveedor = proveedor;
-		this.precio = precio;
-	}
-
-	public Suministra() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	
+	private @Getter @Setter int precio;
 }
